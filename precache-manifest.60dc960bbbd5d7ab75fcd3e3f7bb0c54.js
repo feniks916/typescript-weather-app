e0d69653bdbedbf60b33301908b9328c24f1133c
feniks916 <53240482+feniks916@users.noreply.github.com>
2020-09-14 02:39:45 +0300

    Updates

---
+++ precache-manifest.60dc960bbbd5d7ab75fcd3e3f7bb0c54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e27b48ba93b557cd8ee5777026adbd",
+    "url": "/typescript-weather-app/index.html"
+  },
+  {
+    "revision": "cb0f83f7c8a001b07a6d",
+    "url": "/typescript-weather-app/static/css/2.ba242066.chunk.css"
+  },
+  {
+    "revision": "66cd87bf7f056b86de9b",
+    "url": "/typescript-weather-app/static/css/main.97994220.chunk.css"
+  },
+  {
+    "revision": "cb0f83f7c8a001b07a6d",
+    "url": "/typescript-weather-app/static/js/2.9d8720b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/typescript-weather-app/static/js/2.9d8720b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66cd87bf7f056b86de9b",
+    "url": "/typescript-weather-app/static/js/main.de8b1d9f.chunk.js"
+  },
+  {
+    "revision": "30a0b39c3d3d8beaa431",
+    "url": "/typescript-weather-app/static/js/runtime-main.ecb2d9e4.js"
+  }
+]);
